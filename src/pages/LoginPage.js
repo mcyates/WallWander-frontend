4fdc24e94b10c01bf7b672350665d4bc17f77b0d
Matthew Yates <Mat.C.Yates@gmail.com>
2019-07-01 16:55:02 -0400

    initial login and registration

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,5 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
-const LoginPage = () => {};
+const LoginPage = () => {
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+	const login = (e) => {
+		e.preventDefault();
+		axios
+			.post('/user/login', {
+				email,
+				password
+			})
+			.then((res) => {
+				console.log(res);
+			})
+			.catch((e) => console.error(e));
+	};
+
+	return (
+		<form onSubmit={login} method="post">
+			<div>
+				<label htmlFor="email">Email</label>
+				<input
+					name="email"
+					type="email"
+					placeholder="foo@bar.com"
+					onChange={(e) => setEmail(e.target.value)}
+				/>
+			</div>
+
+			<div>
+				<label htmlFor="password">Password</label>
+				<input
+					name="password"
+					type="password"
+					placeholder="123abc"
+					onChange={(e) => setPassword(e.target.value)}
+					minLength="8"
+				/>
+			</div>
+		</form>
+	);
+};
 
 export default LoginPage;
--- src/pages/RegistrationPage.js
+++ src/pages/RegistrationPage.js
@@ -1,25 +1,44 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 
 const RegistrationPage = () => {
-	const userName = useState('');
-	const email = useState('');
-	const password = useState('');
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+
+	const register = (e) => {
+		e.preventDefault();
+		axios
+			.post('/user/register', {
+				email,
+				password
+			})
+			.then((res) => {
+				console.log(res);
+			})
+			.catch((e) => console.error(e));
+	};
 
 	return (
-		<form method="post">
-			<div>
-				<label htmlFor="username">UserName</label>
-				<input name="username" type="text" placeholder={userName} />
-			</div>
-
+		<form onSubmit={register} method="post">
 			<div>
 				<label htmlFor="email">Email</label>
-				<input name="email" type="email" placeholder={email} />
+				<input
+					name="email"
+					type="email"
+					placeholder="foo@bar.com"
+					onChange={(e) => setEmail(e.target.value)}
+				/>
 			</div>
 
 			<div>
 				<label htmlFor="password">Password</label>
-				<input name="password" type="password" placeholder={password} />
+				<input
+					name="password"
+					type="password"
+					placeholder="123abc"
+					onChange={(e) => setPassword(e.target.value)}
+					minLength="8"
+				/>
 			</div>
 		</form>
 	);
