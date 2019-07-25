e579b5fbd9342d1b3ee03e0e4e17fa11f772aa22
Matthew Yates <Mat.C.Yates@gmail.com>
2019-07-25 13:12:06 -0400

    added footer

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,6 +2,13 @@ import React from 'react';
 import WallpaperList from '../components/WallpaperList';
 
 export const HomePage = () => {
-	return <WallpaperList />;
+	return (
+		<React.Fragment>
+		<WallpaperList />
+		<footer>
+		Created by Matthew Yates, &copy; {new Date().getFullYear()}
+		</footer>
+		</React.Fragment>
+		 );
 };
 export default HomePage;
