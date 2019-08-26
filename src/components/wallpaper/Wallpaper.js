2acb0367f18d98a65d386f797b69f08f39fe5906
Matthew Yates <Mat.C.Yates@gmail.com>
2019-08-26 14:42:47 -0400

    styling changes to wallpaper page

--- src/components/wallpaper/Wallpaper.js
+++ src/components/wallpaper/Wallpaper.js
@@ -9,49 +9,56 @@ export const Wallpaper = (props) => {
 	const { author, id, image, isAuthed, user } = wallpaperData;
 
 	const urlArr = image.secureUrl.split('/');
-	urlArr[6] = 'f_auto,h_2560,w_1440,c_limit,q_auto:best';
+	// urlArr[6] = 'f_auto,h_2560,w_1440,c_limit,q_auto:best';
+	urlArr[6] = 'f_auto,h_1920,c_limit,q_auto:best';
 	const optUrl = urlArr.join('/');
 
 	return (
-		<div className="wallpaper">
-			<figure className="wallpaper--figure">
-				<a href={image.secureUrl}>
-					<img className="wallpaper--img" src={optUrl} alt={image.title} />
-					<figcaption className="wallpaper--info">
+		<div className='wallpaper'>
+			<div className='wallpaper-sidebar'>
+				<div className='wallpaper-container'>
+					<div className='wallpaper-info'>
 						<p>height: {image.height}px</p>
 						<p>width: {image.width}px</p>
 						<p>format: {image.format}</p>
 						<p>Views: {image.views}</p>
-					</figcaption>
-				</a>
-			</figure>
+					</div>
+					<p className='wallpaper-uploader'>
+						uploader:
+						<Link className='nav--link' to={`/profile/${image.userId}`}>
+							{image.authorName}
+						</Link>
+					</p>
+					<div className='wallpaper-actions'>
+						{author ? (
+							<React.Fragment />
+						) : (
+							<Favorite
+								id={id}
+								userId={user.id}
+								user={user}
+								isAuthed={isAuthed}
+							/>
+						)}
 
-			<div className="wallpaper--misc">
-				<TagsLogic id={id} user={user} />
-
-				<div>
-				{author ? (
-					<React.Fragment />
-				) : (
-					<Favorite id={id} userId={user.id} user={user} isAuthed={isAuthed} />
-				)}
-				
-				<p className="wallpaper--uploader">
-				uploader:
-				<Link className="nav--link" to={`/profile/${image.userId}`}>
-				{image.authorName}
-				</Link>
-				</p>
-				
-				{author ? (
-					<button className="btn btn-danger" onClick={removeImage}>
-						Delete
-						</button>
+						{author ? (
+							<button className='btn btn-danger' onClick={removeImage}>
+								Delete
+							</button>
 						) : (
 							<React.Fragment />
-							)}
-							</div>
-							</div>
+						)}
+					</div>
+				</div>
+				<TagsLogic id={id} user={user} />
+			</div>
+			<div className='wallpaper-fit'>
+				<figure className='wallpaper-figure'>
+					<a href={image.secureUrl}>
+						<img className='wallpaper-img' src={optUrl} alt={image.title} />
+					</a>
+				</figure>
+			</div>
 		</div>
 	);
 };
--- src/components/wallpaper/tags/TagsView.js
+++ src/components/wallpaper/tags/TagsView.js
@@ -5,41 +5,41 @@ export const TagsView = (props) => {
 	const { removeTag, submit, tags } = props;
 
 	return (
-		<div className="tags">
+		<div className='tags'>
 			<h3>Tags</h3>
-			<form className="tags-form" onSubmit={submit} method="post">
+			<form className='tags-form' onSubmit={submit} method='post'>
 				<input
-					className="tags-form-input"
-					name="tagText"
-					type="text"
-					placeholder="add tag"
+					className='tags-form-input'
+					name='tagText'
+					type='text'
+					placeholder='add tag'
 					onChange={(e) => props.setTagText(e.target.value)}
 				/>
 				<select
-					defaultValue="false"
+					defaultValue='false'
 					onChange={(e) => props.setTagNsfw(e.target.value)}
-					className="tags-form-select"
+					className='tags-form-select'
 				>
-					<option className="tags-form-option" value="false">
+					<option className='tags-form-option' value='false'>
 						sfw
 					</option>
-					<option className="tags-form-option" value="true">
+					<option className='tags-form-option' value='true'>
 						nsfw
 					</option>
 				</select>
-				<button className="btn-tags-form">Add</button>
+				<button className='btn-tags-form'>Add</button>
 			</form>
-			<ul className="tags-list">
+			<ul className='tags-list'>
 				{tags.map((tag) => {
 					let tagClass = `tags-tag tags-tag-${tag.nsfw}`;
 
 					return (
 						<li className={tagClass} key={tag.id}>
 							<Link to={`/wallpapers/search/?tags=${tag.tag}`}>
-								<p className="tags-tag-data">{tag.tag}</p>
+								<p className='tags-tag-data'>{tag.tag}</p>
 							</Link>
 							<button
-								className="btn-tags-danger"
+								className='btn-tags-danger'
 								onClick={() => removeTag(tag.tag)}
 							>
 								X
